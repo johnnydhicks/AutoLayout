7a32dbf012d0bfce9e0052940bbbdc0bcc1f9eed
Johnny Hicks <johnnydhicks@gmail.com>
2019-05-01 19:58:52 -0600

    Initial commit

---
+++ AutoLayout (iOSPT1)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout (iOSPT1)
+//
+//  Created by Johnny Hicks on 5/1/19.
+//  Copyright © 2019 Johnny Hicks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
